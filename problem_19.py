b37ec61e9cc36ee8d99583098514c9801ee3fbfa
DannnyBoi <ddp11072002@gmail.com>
2026-06-07 22:26:15 +0530

    Problem 19 with 3 dict

--- problem_19.py
+++ problem_19.py
@@ -1,17 +1,33 @@
-fx_rate = {
+fx_rate = [{
     "TXN_ID": "TX001",
     "currency_from": "INR",
     "currency_to": "USD",
     "rate": 83.5,
     "rate_type": "LIVE",
     "provider": "Reuters"
+},
+{
+    "TXN_ID": "TX002",
+    "currency_from": "",
+    "currency_to": "EUR",
+    "rate": 110,
+    "rate_type": "Not_fixed",
+    "provider": "XE"
+},
+{
+    "TXN_ID": "TX003",
+    "currency_from": "",
+    "currency_to": "",
+    "rate": 0,
+    "rate_type": "not_fixed",
+    "provider": "NA"
 }
-
+]
 def validate_fx_rate(fx_rate):
     error = []
-    if not len(fx_rate["currency_from"]):
+    if not fx_rate["currency_from"]:
         error.append("currency_from cannot be empty")
-    if not len(fx_rate["currency_to"]):
+    if not fx_rate["currency_to"]:
         error.append("currency_to cannot be empty")
     if (fx_rate["rate"]) <= 0:
         error.append("rate must be greater than 0")
@@ -20,11 +36,9 @@ def validate_fx_rate(fx_rate):
     if (fx_rate["provider"]) not in ("Reuters", "Bloomberg", "XE"):
         error.append("provider must be one of — Reuters, Bloomberg, XE")
     if error:
-        return((fx_rate["TXN_ID"]) + " : " ", ".join(error))
+        return((fx_rate["TXN_ID"]) + " : " +", ".join(error))
     else:
-        return("Valid Fx rate")
+        return((fx_rate["TXN_ID"]) + " : Valid Fx rate")
 
-"""for fx_rate_check in fx_rate:
-    print(fx_rate_checker(validate_fx_rate))"""
-
-print(validate_fx_rate(fx_rate))
+for fx_rate_check in fx_rate:
+    print(validate_fx_rate(fx_rate_check))
