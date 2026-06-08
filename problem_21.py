8b98082f4923181841034bd60339abb4794b5e01
DannnyBoi <ddp11072002@gmail.com>
2026-06-08 09:49:07 +0530

    Problem 21 3 dict

--- problem_21.py
+++ problem_21.py
@@ -1,31 +1,48 @@
-transaction_fee = {
+transaction_fee = [{
     "txn_id": "TX001",
     "txn_amount": 50000,
     "fee_percentage": 1.5,
     "fee_amount": 750.0,
     "fee_type": "FIXED"
-}
+},
+{
+    "txn_id": "",
+    "txn_amount": 80000,
+    "fee_percentage": 1.7,
+    "fee_amount": 1360.0,
+    "fee_type": "NA"
+},
+{
+    "txn_id": "",
+    "txn_amount": 0,
+    "fee_percentage": 7,
+    "fee_amount": 750.0,
+    "fee_type": "NA"
+}]
 
 def txn_fee_validator(transaction_fee):
     error = []
     if transaction_fee["txn_id"]:
-        id = transaction_fee["txn_id"]
+        txid = transaction_fee["txn_id"]
     else:
-        id = "UNKNOWN"
+        txid = "UNKNOWN"
     
     if not transaction_fee["txn_id"]:
         error.append("txn_id cannot be empty")
     if transaction_fee["txn_amount"] <= 0:
         error.append("txn_amount must be greater than 0")
-    if transaction_fee["fee_percentage"] <= 0.1 and transaction_fee["fee_percentage"] >= 5:
+    if transaction_fee["fee_percentage"] < 0.1 or transaction_fee["fee_percentage"] > 5:
         error.append("fee_percentage must be between 0.1 and 5.0")
     if transaction_fee["fee_amount"] != (transaction_fee["txn_amount"] * transaction_fee["fee_percentage"])/100:
         error.append("fee_amount must equal txn_amount multiplied by fee_percentage divided by 100")
     if transaction_fee["fee_type"] not in ("FIXED", "PERCENTAGE", "HYBRID"):
         error.append("fee_type must be one of — FIXED, PERCENTAGE, HYBRID")
     if error:
-        return(id + " : " + "; ".join(error))
+        return(txid + " : " + "; ".join(error))
     else:
-        return(id + " : Valid fee")
+        return(txid + " : Valid fee")
 
-print(txn_fee_validator(transaction_fee))
+for fee_check in transaction_fee:
+    print(txn_fee_validator(fee_check))
+
+"""print(txn_fee_validator(transaction_fee))"""
