4440bca6dbba74a3f3a89dc1b17008dcf3983aab
DannnyBoi <ddp11072002@gmail.com>
2026-06-10 12:13:52 +0530

    Prob 28A

---
+++ problem_28.py
@@ -0,0 +1,24 @@
+transaction_data = {
+    "transaction_id": "TXN001",
+    "status": "PENDING",
+    "max_attempts": 1
+}
+
+def poll_transaction_status(txn_data):
+    attempt = 1
+    status = txn_data["status"]
+
+    while attempt <= txn_data["max_attempts"]:
+        if attempt == 2:
+            status = "COMPLETED"
+            print(status + " : Transaction completed Successfully")
+            break
+        attempt = attempt + 1
+
+    if status == "COMPLETED":
+        pass
+    else:
+        print("Max attempts reached — Transaction TIMED_OUT")
+    
+
+poll_transaction_status(transaction_data)
