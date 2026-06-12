b8f93ad7f5cde01c16e8d51373b4f350034767d8
DannnyBoi <ddp11072002@gmail.com>
2026-06-12 13:16:45 +0530

    prob 32 b - self trial

---
+++ problem_32b.py
@@ -0,0 +1,21 @@
+print("Self trial")
+
+test_inputs = ["abc", "xyz", "5000"]
+
+def xyz(inp):
+    attempt = 1
+    max_attempt = 3
+    success = False
+    while attempt <= max_attempt:
+        try:
+            conv = float(inp[attempt -1])
+            print("Successful conversion on attempt " + str(attempt))
+            success = True
+            break
+        except:
+            print("Failed to convert on attempt " + str(attempt))
+        attempt = attempt+1
+        
+    if success == False:
+        print("Failed to convert")
+xyz(test_inputs)
